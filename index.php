e1353cdae4d0ec28ce3c1b576738b792712e0f3f
glennpaulmabao <64250849+glennpaulmabao@users.noreply.github.com>
2020-09-11 07:41:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,3 @@
-<?php 
-
-    if(isset($_POST['btn-send']))
-    {
-       $Name = $_POST['Name'];
-       $Email = $_POST['Email'];
-       $Phone = $_POST['Phone'];
-       $Msg = $_POST['msg'];
-
-       if(empty($Name) || empty($Email) || empty($Phone) || empty($Msg))
-       {
-           header('location:index.php?error');
-       }
-       else
-       {
-           $to = "glennfalse.ph@gmail.com";
-
-           if(mail($to,$Phone,$Msg,$Email))
-           {
-               header("location:index.php?success");
-           }
-       }
-    }
-    else
-    {
-        header("location:index.php");
-    }
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
