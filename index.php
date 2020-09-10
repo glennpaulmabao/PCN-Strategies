66e74e9c08e22909486d160a159ef6b54bdc4087
glennpaulmabao <64250849+glennpaulmabao@users.noreply.github.com>
2020-09-10 09:48:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,21 @@
-<?php
-if(isset($_POST["submit"])){
-// Checking For Blank Fields..
-if($_POST["name"]==""||$_POST["email"]==""||$_POST["phone"]==""||$_POST["message"]==""){
-echo "Fill All Fields..";
-}else{
-// Check if the "Sender's Email" input field is filled out
-$email=$_POST['email'];
-// Sanitize E-mail Address
-$email =filter_var($email, FILTER_SANITIZE_EMAIL);
-// Validate E-mail Address
-$email= filter_var($email, FILTER_VALIDATE_EMAIL);
-if (!$email){
-echo "Invalid Sender's Email";
-}
-else{
-$phone = $_POST['phone'];
-$message = $_POST['message'];
-$headers = 'From:'. $email2 . "rn"; // Sender's Email
-$headers .= 'Cc:'. $email2 . "rn"; // Carbon copy to Sender
-// Message lines should not exceed 70 characters (PHP rule), so wrap it
-$message = wordwrap($message, 70);
-// Send Mail By PHP Mail Function
-mail("glennpaulmabao@yahoo.com.ph", $phone, $message, $headers);
-echo "Your mail has been sent successfuly ! Thank you for your feedback";
-}
-}
-}
+<?php 
+if(isset($_POST['submit'])){
+    $to = "glennpaulmabao@yahoo.com.ph"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['name'];
+    $phone = $_POST['phone'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $name . " " . $email . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
 ?>
 
 <!DOCTYPE html>
